9a1995aea24eb8b646e5ab2ec0d7cb668dfd3376
John Gibbons <jwgibbo@gmail.com>
2022-02-09 10:58:41 -0600

    Add notes 268 2022.02.09

---
+++ eecs168/2022spring/TR/2022.02.08.loops/loops.py
@@ -0,0 +1,17 @@
+word = 'barnyardsnarsgard'
+
+index = 0
+while index < len(word):
+    print(word[index])
+    index = index + 1
+     
+print('=========')
+
+
+for letter in word:
+    print(letter)
+    if letter == 'y':
+        break #stops the loop
+    
+    
+
---
+++ eecs268/2022spring/MWF/2022.02.09.stacks/stack.py
@@ -0,0 +1,14 @@
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None
+
+    def is_empty(self):
+        return self._top == None
+
+    def push(???):
+        ???
+
+    def pop(???):
+        ???
