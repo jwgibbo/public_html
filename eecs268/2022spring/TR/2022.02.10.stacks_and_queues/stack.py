c77f1ad9ed93810fbcd89d3a8df335d613c500d3
John Gibbons <jwgibbo@gmail.com>
2022-02-10 12:09:28 -0600

    Add stack and queue notes/code

---
+++ eecs268/2022spring/TR/2022.02.10.stacks_and_queues/stack.py
@@ -0,0 +1,11 @@
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None
+
+    def push(self, entry):
+        #board work 1
+
+    def pop(self):
+        #board work 2
