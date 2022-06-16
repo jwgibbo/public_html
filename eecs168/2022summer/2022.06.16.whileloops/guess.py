da9123660c4a6d134656f8bebca4663699fd28c1
John Gibbons <jwgibbo@gmail.com>
2022-06-16 10:50:42 -0500

    Add 168 notes

---
+++ eecs168/2022summer/2022.06.16.whileloops/guess.py
@@ -0,0 +1,11 @@
+SECRET_NUMBER = 27
+print("Guess the secret number!")
+
+
+#initial guess
+guess = int(input('Enter your guess: '))
+
+#      looping condition
+while guess != SECRET_NUMBER:
+    #progression
+    guess = int(input('Enter your guess: '))
