c4e44fa538690e9529ec3f9d95ee1e476ddbd6b0
John Gibbons <jwgibbo@gmail.com>
2022-06-23 11:54:40 -0500

    Add 268 notes

---
+++ eecs268/2022summer/2022.06.23.linkedlists-part-2/linkedlist.py
@@ -0,0 +1,15 @@
+#linkedlist.py
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        '''Add entry to given index'''
+        
+
+    def get_entry(self, index):
+        '''Return value at the index'''
+        
