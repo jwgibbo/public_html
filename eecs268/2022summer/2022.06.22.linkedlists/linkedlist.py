cc67797e985cf381d2e14417e1ac7d8ea9e5ba6e
John Gibbons <jwgibbo@gmail.com>
2022-06-23 09:58:57 -0500

    Add 168/268 notes

---
+++ eecs268/2022summer/2022.06.22.linkedlists/linkedlist.py
@@ -0,0 +1,14 @@
+#linkedlist.py
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        
+
+    def get_entry(self, index):
+        '''Return value at the index'''
+        
