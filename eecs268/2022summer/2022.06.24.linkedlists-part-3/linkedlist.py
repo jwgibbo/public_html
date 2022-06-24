e314d1183c4c68b3db8e2729eefe8994527cf56a
John Gibbons <jwgibbo@gmail.com>
2022-06-24 11:54:20 -0500

    Add 268 notes

---
+++ eecs268/2022summer/2022.06.24.linkedlists-part-3/linkedlist.py
@@ -0,0 +1,25 @@
+#linkedlist.py
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        '''Add entry to given index'''
+
+    def remove(self, index):
+        '''Remove entry from index'''
+
+    def get_entry(self, index):
+        '''Return value at the index'''
+        
+    def clear(self):
+        '''Empty the list'''
+
+    def length(self):
+        '''Return length of list'''
+
+    def count(self, target):
+        '''Count occurences of target'''
