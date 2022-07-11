c4938b71219174d5a913a8c101bb152351c589ff
John Gibbons <jwgibbo@gmail.com>
2022-07-11 10:55:07 -0500

    Add 168 notes

---
+++ eecs168/2022summer/2022.07.11.functions/functiondemo.py
@@ -0,0 +1,11 @@
+#Goal: Write a function that can take
+#      a number and calculate what
+#       1 more than that number is
+def one_more(num):
+    ans = num + 1
+    return ans
+
+
+
+x = one_more(5) #function call
+print(x)
