869dbe80369d53d22701c28b0b160c7b1289d3ad
John Gibbons <jwgibbo@gmail.com>
2022-08-29 11:51:46 -0500

    add 168 notes

---
+++ eecs168/2022fall/MWF/2022.08.29.variables/variabledemo.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+name = input("Enter your name: ")
+
+
+print("Your name is", name)
+print("Current age:", age)
+
+
+print("name's type:", type(name))
+print("age's type:", type(age))
+
+print("Age when life improves:", age+15)
