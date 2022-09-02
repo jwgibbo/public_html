7508be4e68711b5417b3298089c51a5015887a58
John Gibbons <jwgibbo@gmail.com>
2022-09-02 09:58:26 -0500

    add 168 notes

---
+++ eecs168/2022fall/TR/2022.09.01.ifs/ifsdemo.py
@@ -0,0 +1,12 @@
+temperature = 55
+
+if temperature > 67:
+    print("Too hot!")
+    print("Crank the A/C")
+    print("Bury me in ice!")
+else:
+    print("Ahh! Sweater weather.")
+    print("Time for 37 pumpkin spice lattes")    
+
+    
+print("Program ending...")
