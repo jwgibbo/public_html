c3d1107fd58388f61841e30bdb7a137020c7263f
John Gibbons <jwgibbo@gmail.com>
2022-09-08 09:27:57 -0500

    add 268 notes

---
+++ eecs268/2022fall/12pm/2022.09.07.notes/queue.py
@@ -0,0 +1,20 @@
+#queue.py
+
+from node import Node
+
+class Queue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def enqueue(self, entry):
+        #place the entry in a Node
+        #add it to the back
+        #make sure to check for
+        #enqueueing on an empty queue
+
+    def is_empty(self):
+        #is the queue empty?
+
+    def dequeue(self):
+        #remove the front
---
+++ eecs268/2022fall/1pm/2022.09.07.queues/queuedemo.py
@@ -0,0 +1,18 @@
+#queue.py
+
+from node import Node
+
+class Queue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def is_empty(self):
+        #is the queue empty?
+
+    def enqueue(self, entry):
+        #place the entry in a node
+        #add the node to the back
+
+    def dequeue(self):
+        #remove the front
