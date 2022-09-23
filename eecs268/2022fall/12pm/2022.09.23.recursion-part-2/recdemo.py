8256e3cc52ce7060947705883982852c16bdbc3a
John Gibbons <jwgibbo@gmail.com>
2022-09-23 12:54:21 -0500

    Add 268 notes

---
+++ eecs268/2022fall/12pm/2022.09.23.recursion-part-2/recdemo.py
@@ -0,0 +1,22 @@
+def rec_func(i):
+    if i >= 3:
+        return
+    else:
+        rec_func(i+1) #recursive call
+        print(i)
+
+
+#Goal: Write a recursive factorial
+def rec_fact(num):
+    if num <= 1:
+        return 1
+    else:
+        return num*rec_fact(num-1)
+
+
+def main():
+    #rec_func(0) #initial call
+
+    print(rec_fact(10))
+
+main()
