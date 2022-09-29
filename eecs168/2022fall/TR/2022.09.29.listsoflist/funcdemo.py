fa8291b528bee83435b3c52c9d53af2fe345dbbf
John Gibbons <jwgibbo@gmail.com>
2022-09-29 10:50:48 -0500

    Add 168 268 notes

---
+++ eecs168/2022fall/TR/2022.09.29.listsoflist/funcdemo.py
@@ -0,0 +1,7 @@
+def add_one(num):
+    ans = num + 1
+    return ans
+
+print('Program running')
+num = add_one(5)
+print(num)
---
+++ eecs168/2022fall/TR/2022.09.29.listsoflist/shell_output.py
@@ -0,0 +1,66 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nums = [10, 20, 30]
+>>> len(nums)
+3
+>>> nums[0]
+10
+>>> nums = 'catfood'
+>>> nums
+'catfood'
+>>> age = 38
+>>> age = 39
+>>> nums
+'catfood'
+>>> nums = [10, 20, 30]
+>>> nums
+[10, 20, 30]
+>>> nums[1] = 99
+>>> nums
+[10, 99, 30]
+>>> grid = []
+>>> grid
+[]
+>>> len(grid)
+0
+>>> grid.append( [10, 20, 30] )
+>>> len(grid)
+1
+>>> grid
+[[10, 20, 30]]
+>>> grid[0]
+[10, 20, 30]
+>>> grid.append([40, 50, 60])
+>>> grid
+[[10, 20, 30], [40, 50, 60]]
+>>> len(grid)
+2
+>>> len(grid[0])
+3
+>>> type(grid)
+<class 'list'>
+>>> type(grid[1])
+<class 'list'>
+>>> grid[1][2]
+60
+>>> grid[2][1]
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    grid[2][1]
+IndexError: list index out of range
+>>> int('30\n')
+30
+>>> int('  30')
+30
+>>> int(' 30 ')
+30
+>>> 
+= RESTART: C:/Users/jwgibbo/Desktop/public_html/eecs168/2022fall/MWF/2022.09.29.listsoflist/funcdemo.py
+>>> 
+= RESTART: C:/Users/jwgibbo/Desktop/public_html/eecs168/2022fall/MWF/2022.09.29.listsoflist/funcdemo.py
+Program running
+>>> 
+= RESTART: C:/Users/jwgibbo/Desktop/public_html/eecs168/2022fall/MWF/2022.09.29.listsoflist/funcdemo.py
+Program running
+6
+>>> 
