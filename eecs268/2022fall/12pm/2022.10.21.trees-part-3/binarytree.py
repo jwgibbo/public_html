59bbee799c97bf60a349f109998a2af05b42955d
John Gibbons <jwgibbo@gmail.com>
2022-10-24 19:36:26 -0500

    Add 168/268 notes

--- eecs268/2022fall/12pm/2022.10.21.trees-part-3/binarytree.py
+++ eecs268/2022fall/12pm/2022.10.21.trees-part-3/binarytree.py
@@ -1,25 +1,26 @@
 #binarytree.py
 from bnode import BNode
-
 class BinaryTree:
     def __init__(self):
         self._root = None
 
+    #Assume this works
     def add(self, entry):
-        #Assume beautiful working
-        #definition here
+        #Awesome, bugfree, definition
 
-    #Public facing method
+    #public facing method
     def is_in(self, target):
         return self._rec_is_in(self, target, self._root)
 
-    #Hidden recursive method
-    def _rec_is_in(self, target, cur_node):
+    def _rec_is_in(self, target, cur_node):  
         if cur_node == None:
             return False
         elif cur_node.entry == target:
             return True
         else:
             is_in_LST = self._rec_is_in(target, cur_node.left)
-            is_in_RST = self._rec_is_in(target, cur_node.right
+            is_in_RST = self._rec_is_in(target, cur_node.right)
             return is_in_LST or is_in_RST
+            
+     def  _rec_node_count(self, cur_node):  
+        
