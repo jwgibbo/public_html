b2d17f2765671d305ef1aea8db8016027f6bf623
John Gibbons <jwgibbo@gmail.com>
2023-01-23 13:50:42 -0600

    Add 168/268 notes

---
+++ eecs168/2023spring/MWF/2023.01.23.input/inputdemo.py
@@ -0,0 +1,3 @@
+user_name = input("Enter your name: ")
+print("Hello", user_name)
+
---
+++ eecs168/2023spring/MWF/2023.01.23.input/printdemo.py
@@ -0,0 +1,8 @@
+print('hello, world')
+print("hello, world")
+print(5+2*10)
+print("I don't know")
+print('He said "Hello."')
+print("He said, \"I don't know.\"")
+print("MWF:\n\tEECS 168")
+
