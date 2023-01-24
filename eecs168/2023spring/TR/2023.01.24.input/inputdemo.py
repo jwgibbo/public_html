0261a5d3c8b68fec862228875f1406377b403415
John Gibbons <jwgibbo@gmail.com>
2023-01-24 14:15:01 -0600

    Add 168/268 notes

---
+++ eecs168/2023spring/TR/2023.01.24.input/inputdemo.py
@@ -0,0 +1,4 @@
+user_name = input("Enter your name: ")
+print("Hello", user_name)
+user_num = int(input("Enter a number to cube: "))
+print(user_num**3)
---
+++ eecs168/2023spring/TR/2023.01.24.input/variabledemo.py
@@ -0,0 +1,11 @@
+num = 5
+word = 'plunge'
+double_num = 2*num
+print(num)
+print(word)
+print(double_num)
+
+base = 10.5
+height = 5.5
+area = base*height*(1/2)
+print(area)
