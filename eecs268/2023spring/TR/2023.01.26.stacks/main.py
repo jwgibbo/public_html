ee3b6da09fae3b604ce90b5b007b3a7d4487b606
John Gibbons <jwgibbo@gmail.com>
2023-01-26 12:22:37 -0600

    add 168/268 notes

---
+++ eecs268/2023spring/TR/2023.01.26.stacks/main.py
@@ -0,0 +1,13 @@
+#main.py
+from node import Node
+
+def main():
+    first = Node("A")
+    temp = Node("B")
+    #Board work finish main to
+    #create and assemble nodes in the
+    #notes
+    front.next = temp
+    temp = Node("C")
+    first.next.next = temp
+    
---
+++ eecs268/2023spring/TR/2023.01.26.stacks/stack.py
@@ -0,0 +1,17 @@
+#stack.py
+from node import Node
+class Stack:
+    def __init__(self):
+        self._top = None #empty stack
+
+    def push(self, entry):
+        ???
+
+    def pop(self):
+        ???
+
+    def peek(self):
+        ???
+
+    def is_empty(self):
+        return self._top == None
