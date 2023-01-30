f134cbb7a74798b77d882fa7e7d9849447147c11
John Gibbons <jwgibbo@gmail.com>
2023-01-30 13:52:11 -0600

    Add 168 notes

---
+++ eecs168/2023spring/MWF/2023.01.30.ifs/ifdemo.py
@@ -0,0 +1,15 @@
+user_num = int(input("Enter a number: "))
+
+if user_num > 0:
+    print('You entered a positive number.')
+    print('Such a positive person!')
+elif user_num == 0:
+    print('You entered zero')
+else:
+    print('You entered a negative number.')
+    
+    
+
+print('Program ending...')
+
+          
