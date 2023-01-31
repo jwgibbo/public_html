d93d8b6d9fb260053e1a47ec0d21e9c7db02409b
John Gibbons <jwgibbo@gmail.com>
2023-01-31 12:22:42 -0600

    Add 268 notes

---
+++ eecs268/2023spring/TR/2023.01.31.stacks-part-2/stack.py
@@ -0,0 +1,19 @@
+#stack.py
+from node import Node
+class Stack:
+    def __init__(self):
+        self._top = None #empty stack
+
+    def push(self, entry):
+        cur_top = self._top
+        #board work, finish push
+        
+
+    def pop(self):
+        ???
+
+    def peek(self):
+        ???
+
+    def is_empty(self):
+        return self._top == None
