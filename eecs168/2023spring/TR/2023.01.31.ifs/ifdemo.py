25c39857750803bf378b6d6dd6dc060eb58e3a9f
John Gibbons <jwgibbo@gmail.com>
2023-01-31 14:14:27 -0600

    Add 168 notes

---
+++ eecs168/2023spring/TR/2023.01.31.ifs/ifdemo.py
@@ -0,0 +1,12 @@
+num = -5
+
+if num > 0:
+    print('You have a postive number!')
+    print('Such a positive person!')
+elif num == 0:
+    print('You got no cakes, no shakes, you got zero!')
+else:
+    print('You have a negative number.')
+
+    
+print('Program ending...')
