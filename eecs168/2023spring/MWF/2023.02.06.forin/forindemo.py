ac34f3ce2b30530d65b27c17dbf54e5873a0d8d2
John Gibbons <jwgibbo@gmail.com>
2023-02-07 14:11:36 -0600

    Add 168/268 notes

---
+++ eecs168/2023spring/MWF/2023.02.06.forin/forindemo.py
@@ -0,0 +1,19 @@
+#Goal: Print the letters of a string
+
+word = 'I love volcano!'
+for letter in word:
+    print(letter)
+
+print('=====')
+
+#Goal: Print the even numbers from
+#       2 to 24
+for num in range(2, 25, 2):
+    print(num)
+    
+print('=====')
+
+#Goal: Print the multiples of 7 from
+    #   14 to 49
+for num in range(14, 50, 7):
+    print(num)
---
+++ eecs168/2023spring/TR/2023.02.07.more-loops/forindemo.py
@@ -0,0 +1,12 @@
+word = 'alfredo'
+
+for letter in word:
+    print(letter)
+print('=====')
+''' Sorry, ints are not iterable
+num = 923874
+for digit in num:
+    print(digit)
+'''
+for num in range(5, 15):
+    print(num)
