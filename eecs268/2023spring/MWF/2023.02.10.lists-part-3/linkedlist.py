af3cdbd2779938c4bc4a87e06f197150f20cb151
John Gibbons <jwgibbo@gmail.com>
2023-02-10 11:11:13 -0600

    Add 268 notes

---
+++ eecs268/2023spring/MWF/2023.02.10.lists-part-3/linkedlist.py
@@ -0,0 +1,18 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        #We're doing this!
+
+    def get_entry(self, index):
+        #make sure index is valid
+        #if not, raise IndexError
+        
+        #where jumper starts
+        jumper = self._front
