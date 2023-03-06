5d1fcf26dcd6e5b9d8165aa9fc3fdcadffb5845f
John Gibbons <jwgibbo@gmail.com>
2023-03-06 13:50:21 -0600

    Add 168 notes

---
+++ eecs168/2023spring/MWF/2023.03.06.functions-part-1/funcdemo.py
@@ -0,0 +1,11 @@
+#GOAL: Define a function that takes
+#       in a number and returns that
+#       number plus one
+def one_more(num):
+    answer = num + 1
+    return answer
+
+print('Welcome!')
+result = one_more(5)
+print(result)
+print('Program ending...')
