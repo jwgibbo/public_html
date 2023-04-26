ffb1aa55d9fd439fb97f9fdd8ba07e8271e45dac
John Gibbons <jwgibbo@gmail.com>
2023-04-26 10:51:59 -0500

    Add 268 notes

---
+++ eecs268/2023spring/MWF/2023.04.26.inheritance/dog.py
@@ -0,0 +1,8 @@
+#dog.py
+
+from animal import Animal
+
+#The Dog class inherits from the Animal class
+class Dog(Animal):
+    def __init__(self):
+        pass
---
+++ eecs268/2023spring/MWF/2023.04.26.inheritance/main.py
@@ -0,0 +1,16 @@
+#main.py
+
+from animal import Animal
+from dog import Dog
+
+def main():
+    my_animal = Animal()
+    my_dog = Dog()
+    
+    my_animal.eat()
+    my_animal.sleep()
+    my_dog.eat()
+    my_dog.sleep()
+    
+
+main()
