8b72dc8ff8b66e243fdbf5cafbe8a011d6617941
John Gibbons <jwgibbo@gmail.com>
2023-06-09 12:51:45 -0500

    Add 268 notes

---
+++ eecs268/2023summer/2023.06.09.linked-structures/main.py
@@ -0,0 +1,16 @@
+#main.py
+
+from node import Node
+
+def main():
+    first = Node('A')
+    temp = ???
+    
+    print(first.entry)
+    print(first.next)
+    print(type(first))
+    print(type(first.entry))
+    print(type(first.next))
+    
+
+main()
