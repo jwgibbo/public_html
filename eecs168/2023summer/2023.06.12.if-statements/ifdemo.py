2c780bfed46829b3d713d14414fb2160cee1d158
John Gibbons <jwgibbo@gmail.com>
2023-06-12 11:54:14 -0500

    Add 168/268 notes

---
+++ eecs168/2023summer/2023.06.12.if-statements/ifdemo.py
@@ -0,0 +1,10 @@
+num = 0
+
+if num > 0:
+    print('num is positive')
+    print('That is great!')
+else:
+    print('num is not positive')
+    print('what a bummer :(')
+
+print('Program ending...')
---
+++ eecs268/2023summer/2023.06.12.stacks/stack.py
@@ -0,0 +1,10 @@
+#stack.py
+
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None #empty stack
+
+    def push(self, entry):
+        #define push
