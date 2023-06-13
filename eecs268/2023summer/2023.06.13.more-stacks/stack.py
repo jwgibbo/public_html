66ae43ab7352e8042db4de27b9ae79c51df96a94
John Gibbons <jwgibbo@gmail.com>
2023-06-13 11:51:49 -0500

    Add 168/268 notes

---
+++ eecs268/2023summer/2023.06.13.more-stacks/stack.py
@@ -0,0 +1,16 @@
+#stack.py
+
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None #empty stack
+
+    def push(self, entry):
+        
+
+    def pop(self):
+        #Define push
+        #If stack is empty raise RuntimeError
+        #otherwise return the popped value
+        
