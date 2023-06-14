283ccad85d6fced1764365d3acfce6d32958402c
John Gibbons <jwgibbo@gmail.com>
2023-06-14 11:27:37 -0500

    Add 168/268 notes

---
+++ eecs168/2023summer/2023.06.14.loops-intro/whiledemo.py
@@ -0,0 +1,6 @@
+#Goal print the value 1 to 5
+num = 1
+
+while num <= 500:
+    print(num)
+    num = num + 1
---
+++ eecs268/2023summer/2023.06.14.queues/linkedqueue.py
@@ -0,0 +1,13 @@
+#linkedqueue.py
+
+from node import Node
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def enqueue(self, entry):
+        #define enqueue
+
+    
