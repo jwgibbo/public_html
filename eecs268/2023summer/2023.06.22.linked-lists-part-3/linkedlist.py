c9c2455ed47a60196b1d417fac103b26c7d3d01e
John Gibbons <jwgibbo@gmail.com>
2023-06-22 12:29:58 -0500

    Update linkedlist.py

--- eecs268/2023summer/2023.06.22.linked-lists-part-3/linkedlist.py
+++ eecs268/2023summer/2023.06.22.linked-lists-part-3/linkedlist.py
@@ -8,27 +8,11 @@ class LinkedList:
         self._length = 0
 
     def insert(self, index, entry):
-        if index < 0 or index > self._length:
-            raise IndexError
-        elif index == 0:
-            new_node = Node(entry)
-            new_node.next = self._front
-            self._front = new_node
-            self._length += 1
-        else:
-            new_node = Node(entry)
-            previous = self._front
+        #See last class notes
 
-            #move previous to the index
-            #before the insert point
-            for i in range(index-1):
-                previous = prevous.next
+    def remove(self, index):
+        #Your definition here
 
-            #Might be a node or None
-            #Either is fine
-            target = previous.next
-
-            #insert the node
-            previous.next = new_node
-            new_node = target
-            self._length += 1
+    def clear(self):
+        self._front = None
+        self._length = 0
