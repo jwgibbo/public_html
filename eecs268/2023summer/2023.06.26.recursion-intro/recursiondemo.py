64a7ccd744614859771697d4b8dabc14f85a64bd
John Gibbons <jwgibbo@gmail.com>
2023-06-26 11:56:13 -0500

    Add 168/268 notes

---
+++ eecs268/2023summer/2023.06.26.recursion-intro/recursiondemo.py
@@ -0,0 +1,14 @@
+def rec_func(num):
+    if num < 4:
+        rec_func(num+1) #recursive call
+        print(num)
+        
+
+def main():
+    print('Program started...')
+    
+    rec_func(0) #initial call
+
+    print('Program ending...')
+
+main()
