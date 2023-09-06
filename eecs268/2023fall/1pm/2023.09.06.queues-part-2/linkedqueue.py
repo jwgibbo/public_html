b297da2471fb06178185dcd49ca64a430b21c356
John Gibbons <jwgibbo@gmail.com>
2023-09-06 13:53:45 -0500

    Add 268 notes

---
+++ eecs268/2023fall/1pm/2023.09.06.queues-part-2/linkedqueue.py
@@ -0,0 +1,13 @@
+#linkedqueue.py
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def enqueue(self, entry):
+        #define
+
+    def dequeue(self):
+        #Friday!
+        
