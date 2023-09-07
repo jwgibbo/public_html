ad19f6479dce402563cb4ee38a2ada159385150c
John Gibbons <jwgibbo@gmail.com>
2023-09-07 10:24:54 -0500

    Add 168 notes

---
+++ eecs168/2023fall/TR/2023.09.07.more-loops/forinexercises.py
@@ -0,0 +1,7 @@
+sum_of_7s = 0
+
+for num in range(7, 71, 7):
+    sum_of_7s = sum_of_7s + num
+    print('num =', num, '\tsum =', sum_of_7s)
+
+print('sum =', sum_of_7s)
---
+++ eecs168/2023fall/TR/2023.09.07.more-loops/userquit.py
@@ -0,0 +1,6 @@
+user_letter = input('Type "q/Q" to quit: ')
+while user_letter != 'q' or user_letter != 'Q':
+    user_letter = input('Sorry, if you want to enter a "q": ')
+
+
+print('Goodbye')
