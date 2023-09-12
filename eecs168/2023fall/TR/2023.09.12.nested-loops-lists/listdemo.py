6e89c669dbc14e6f513120544ddee2954aa7c357
John Gibbons <jwgibbo@gmail.com>
2023-09-12 10:47:08 -0500

    Add 168 notes

---
+++ eecs168/2023fall/TR/2023.09.12.nested-loops-lists/listdemo.py
@@ -0,0 +1,24 @@
+#Goal: Obtain 5 ints from the user
+
+age = 39
+nums = []
+
+nums.append(5)
+nums.append(age)
+
+print(nums[0])
+
+print(nums[1]+20)
+
+print('=========')
+
+candies = []
+candies.append('gummy worms')
+candies.append('kit kats')
+candies.append('candy corn')
+candies.append('bit-o-honey')
+
+for candy in candies:
+    print('I love', candy)
+
+
---
+++ eecs168/2023fall/TR/2023.09.12.nested-loops-lists/nesteddemo.py
@@ -0,0 +1,5 @@
+for i in range(1, 4):
+    for j in range(10, 16):
+        print('i =', i, '\tj =', j)
+
+    print('Inner loop done')
