c352de07a33bf8ed349d5610ffc80522f89dd035
John Gibbons <jwgibbo@gmail.com>
2023-11-17 10:55:18 -0600

    Add 168 notes

---
+++ eecs168/2023fall/MWF/2023.11.17.exercises/breakdemo.py
@@ -0,0 +1,13 @@
+num = 1
+
+while True:
+    print(num)
+    num += 1
+    if num >= 5:
+        break #stops loop
+
+
+for letter in 'bananarama':
+    print(letter)
+    if letter == 'r':
+        break
