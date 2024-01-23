a301cea9bf76f6469f2475c442054b86b854f288
John Gibbons <jwgibbo@gmail.com>
2024-01-23 14:15:12 -0600

    Add 168 notes

---
+++ eecs168/2024spring/TR/2024.01.23.input-output/printdemo.py
@@ -0,0 +1,4 @@
+print("I didn't check")
+print(27)
+print('27')
+print('1000*75/3 =', 1000*75/3)
---
+++ eecs168/2024spring/TR/2024.01.23.input-output/variabledemo.py
@@ -0,0 +1,6 @@
+year = 2024
+print(year)
+print(3000-year)
+
+first_name = input('Enter your name: ')
+print(first_name)
