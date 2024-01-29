002f2a585199ebc2efe2ebb1dd997bbea79fdf67
John Gibbons <jwgibbo@gmail.com>
2024-01-29 10:51:37 -0600

    Add 268 notes

---
+++ eecs268/2024spring/MWF/2024.01.29.stacks-part-2/stack.py
@@ -0,0 +1,18 @@
+#stack.py
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None
+
+    def push(self, entry):
+        #define (friday)
+
+    def pop(self):
+        #define (monday)
+
+    #Let's define is_empty
+    def is_empty(self):
+        return self._top is None
+
+    
