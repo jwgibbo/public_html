32782709623b14133aaf2f748afd398404479274
John Gibbons <jwgibbo@gmail.com>
2024-01-30 14:17:07 -0600

    Add 168/268 notes

---
+++ eecs268/2024spring/TR/2024.01.30.stacks-part-2/stack.py
@@ -0,0 +1,20 @@
+#stack.py
+
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None
+
+    def push(self, entry):
+        ???
+
+    def pop(self):
+        ???
+
+    def is_empty(self):
+        if self._top is None:
+            return True
+        
+
+    
