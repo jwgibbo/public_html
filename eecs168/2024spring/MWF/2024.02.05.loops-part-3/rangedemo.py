ac5ef9e45749a6f23b5ff7de824520b9ecaf3968
John Gibbons <jwgibbo@gmail.com>
2024-02-05 13:55:17 -0600

    Add 168 notes

---
+++ eecs168/2024spring/MWF/2024.02.05.loops-part-3/rangedemo.py
@@ -0,0 +1,19 @@
+for num in range(5):
+    print(num)
+
+print('=========')
+
+for num in range(5,10):
+    print(num)
+
+print('=========')
+
+for num in range(3, 10, 4):
+    print(num)
+
+print('=========')
+total = 0
+for num in range(1, 6):
+    total = ???
+
+print(total)
