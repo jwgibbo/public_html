7e9f2508fa4c40e577dff342e60774e95503b0f9
John Gibbons <jwgibbo@gmail.com>
2024-02-09 10:55:15 -0600

    add 168/268 notes

---
+++ eecs168/2024spring/TR/2024.02.08.string-lists/listdemo.py
@@ -0,0 +1,10 @@
+#listdemo.py
+
+nums = [2, 8, 24]
+print(nums[1])
+nums.append(99)
+print(nums)
+nums.insert(2, 55)
+print(nums)
+nums.pop(1)
+print(nums)
---
+++ eecs268/2024spring/MWF/2024.02.09.lists-part-2/linkedlist.py
@@ -0,0 +1,14 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def get_entry(self, index):
+        #defined last class
+
+    def insert(self, index, entry):
+        #define this
