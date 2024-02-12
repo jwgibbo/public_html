4e2c3fbb5017aa74d844a6a647b36170ff48b195
John Gibbons <jwgibbo@gmail.com>
2024-02-12 14:02:25 -0600

    Add 168 notes

--- eecs168/2024spring/MWF/2024.02.05.loops-part-3/rangedemo.py
+++ eecs168/2024spring/MWF/2024.02.05.loops-part-3/rangedemo.py
@@ -8,12 +8,12 @@ for num in range(5,10):
 
 print('=========')
 
-for num in range(3, 10, 4):
+for num in range(3, 10, 2):
     print(num)
 
 print('=========')
 total = 0
 for num in range(1, 6):
-    total = ???
+    total = total + num
 
 print(total)
