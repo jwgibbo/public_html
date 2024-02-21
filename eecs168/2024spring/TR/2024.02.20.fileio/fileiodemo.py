59382e45572f2e35f339ed8cd6dbec2b906f0230
John Gibbons <jwgibbo@gmail.com>
2024-02-21 10:50:28 -0600

    Add 168/268 notes

---
+++ eecs168/2024spring/TR/2024.02.20.fileio/fileiodemo.py
@@ -0,0 +1,8 @@
+#filedemo.py
+
+recipe_file = open('recipe.txt', 'r')
+
+for line in recipe_file:
+    print(line.strip())
+
+recipe_file.close()
---
+++ eecs168/2024spring/TR/2024.02.20.fileio/writedemo.py
@@ -0,0 +1,5 @@
+output_file = open('output.txt', 'w')
+
+output_file.write('testing, testing')
+
+output_file.close()
