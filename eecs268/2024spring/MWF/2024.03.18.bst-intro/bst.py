9ad4ff1a106a50399affb0326a81cd3043a0fa7e
John Gibbons <jwgibbo@gmail.com>
2024-03-18 11:00:47 -0500

    Add 268 notes

---
+++ eecs268/2024spring/MWF/2024.03.18.bst-intro/bst.py
@@ -0,0 +1,25 @@
+#bst.py
+
+from binarynode import BinaryNode
+
+class BST:
+    def __init__(self):
+        self._root = None
+
+    def add(self, entry):
+        #Call a "private/hidden" recursive
+        #method
+        if self._root is None:
+            self._root = BinaryNode(entry)
+        else:
+            self._rec_add(entry, self._root)
+
+    def _rec_add(self, entry, cur_node):
+        #recursive traverse throught the tree
+        #to find a spot to put a new node with
+        #the new value OR raise an exception
+        #if the value is a duplicate
+        if entry < cur_node.entry:
+            #Add it if the LST is empty
+            #Otherwise, recurse into LST
+           
