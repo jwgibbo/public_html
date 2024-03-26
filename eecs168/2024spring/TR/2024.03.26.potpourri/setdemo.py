b82558d26cdb991116d638914ea794db93ca739d
John Gibbons <jwgibbo@gmail.com>
2024-03-26 14:28:27 -0500

    Add 168 notes

---
+++ eecs168/2024spring/TR/2024.03.26.potpourri/setdemo.py
@@ -0,0 +1,18 @@
+your_word = 'fruit'
+my_word = 'tripper'
+
+your_set = set(your_word)
+print(your_set)
+
+my_set = set(my_word)
+print(my_set)
+
+print(my_set.intersection(your_set))
+print(my_set.union(your_set))
+print(my_set.difference(your_set))
+print(your_set.difference(my_set))
+
+your_set.add('z')
+print(your_set)
+your_set.remove('z')
+print(your_set)
---
+++ eecs168/2024spring/TR/2024.03.26.potpourri/tupdemo.py
@@ -0,0 +1,12 @@
+my_tup = (5, 10, 15, 20)
+print(my_tup)
+print(len(my_tup))
+print(type(my_tup))
+
+for num in my_tup:
+    print(num)
+
+my_info = ('john', 12345)
+employees = {}
+employees[my_info] = 'EECS'
+print(employees)
