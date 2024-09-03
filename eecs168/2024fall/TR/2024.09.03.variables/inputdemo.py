932ed8293d91aff7323803a7cf6384ccb681ad8c
John Gibbons <jwgibbo@gmail.com>
2024-09-03 10:42:45 -0500

    Add 168 notes

---
+++ eecs168/2024fall/TR/2024.09.03.variables/inputdemo.py
@@ -0,0 +1,9 @@
+user_name = input('What is your name?: ')
+print('Hello', user_name)
+print(type(user_name))
+
+user_age = int(input('How old are you?: '))
+print(type(user_age))
+print('You are', user_age, 'years old')
+age_in_3000 = 976 + user_age
+print('In the year 3000 you will be', age_in_3000, 'years old')
---
+++ eecs168/2024fall/TR/2024.09.03.variables/variabledemo.py
@@ -0,0 +1,5 @@
+flush_with_$$$ = 'happiness'
+city = 'Kansas City'
+distance = 45
+print(city, 'is', distance, 'miles away')
+print('city')
