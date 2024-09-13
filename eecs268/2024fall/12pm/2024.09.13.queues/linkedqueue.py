495b0c82f285cdcd895483397a8c1a75b0a5ee84
John Gibbons <jwgibbo@gmail.com>
2024-09-13 13:47:12 -0500

    Add 268 notes

---
+++ eecs268/2024fall/12pm/2024.09.13.queues/linkedqueue.py
@@ -0,0 +1,15 @@
+#linkedqueue.py
+#NOTE: python already has a 'queue' module
+
+from node import Node
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def is_empty(self):
+        return self._front is None and self._back is None:
+
+    def enqueue(self, entry):
+        #define this
