bc5860e8b23fa408228ef02ef2edeacec129c60b
John Gibbons <jwgibbo@gmail.com>
2024-09-20 10:49:25 -0500

    Add 168 notes

---
+++ eecs168/2024fall/MWF/2024.09.20.for-loops/sumdemo.py
@@ -0,0 +1,10 @@
+#Goal: Calculate the summation of 1 to 5
+total = 0
+print('num\ttotal')
+      
+for num in range(1,6):
+    total = total + num
+    print(f'{num}\t{total}')
+
+print('--------')
+print(total)
