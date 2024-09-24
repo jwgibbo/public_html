ca038408ce34fa5f7f901f6ea6fcab57469be8c4
John Gibbons <jwgibbo@gmail.com>
2024-09-24 09:26:30 -0500

    Add 268 notes

---
+++ eecs268/2024fall/12pm/2024.09.23.linked-lists/linkedlist.py
@@ -0,0 +1,23 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def length(self):
+        return self._length
+
+    def remove(self, index):
+        #define this now (Monday)
+
+    def insert(self, index, entry):
+        #define this third
+
+    def get_entry(self, index):
+        #define this first
+
+    def set_entry(self, index, new_entry):
+        #define this second
---
+++ eecs268/2024fall/1pm/2024.09.23.linked-lists/linkedlist.py
@@ -0,0 +1,21 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def length(self):
+        return self._length
+
+    def insert(self, index, entry):
+        #define this (Friday)
+
+    def get_entry(self, index):
+        #define this first
+
+    def set_entry(self, index, new_entry):
+        #define this second
+        #It replaces the entry at the index
