cb46aee803f5112fdc207e3f579a6382a4eb288a
John Gibbons <jwgibbo@gmail.com>
2024-09-25 10:56:31 -0500

    Add 168 notes

---
+++ eecs168/2024fall/MWF/2024.09.25.nest-loops/griddemo.py
@@ -0,0 +1,5 @@
+#Goal: Print a 4x3 grid of $
+for row in range(1, 5):
+    for col in range(1, 4):
+        print('$', end='')
+    print('')
---
+++ eecs168/2024fall/MWF/2024.09.25.nest-loops/nesteddemo.py
@@ -0,0 +1,6 @@
+print('outer_num\tinner_num')
+for outer_num in range(1, 4):
+    for inner_num in range(11, 15):
+        print(f'{outer_num}\t\t{inner_num}')
+    print('==INNER LOOP ENDING==')
+        
