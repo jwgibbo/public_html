58399bd4af994799b295e6c81507b3a09c0a7a79
John Gibbons <jwgibbo@gmail.com>
2024-10-10 10:52:22 -0500

    Add 168 notes

--- eecs168/2024fall/TR/2024.10.08.fileio/matrixdemo/matrixdemo.py
+++ eecs168/2024fall/TR/2024.10.08.fileio/matrixdemo/matrixdemo.py
@@ -5,7 +5,6 @@ grid_file = open('datagrid.txt', 'r')
 for line in grid_file:
     stripped_line = line.strip()
     split_line = stripped_line.split(' ')
-
     #iterate through the list of strings
     for string in split_line:
         number = int(string)
---
+++ eecs168/2024fall/TR/2024.10.10.functions/functiondemo.py
@@ -0,0 +1,11 @@
+#function defintion
+def add_one(num):
+    answer = num + 1
+    return answer
+
+#function call
+print(add_one(5))
+
+result = add_one(99)
+print(result)
+    
