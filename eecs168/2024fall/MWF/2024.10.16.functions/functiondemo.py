cde3d7e80c5ae32d0da77b9552ab76117837072a
John Gibbons <jwgibbo@gmail.com>
2024-10-16 10:52:28 -0500

    Add 168 notes

---
+++ eecs168/2024fall/MWF/2024.10.16.functions/functiondemo.py
@@ -0,0 +1,9 @@
+#Goal: Write a function that takes a number
+#       and returns that number plus one
+def add_one(num):
+    answer = num + 1
+    return answer
+
+#        function call
+result = add_one(5)
+print(result)
