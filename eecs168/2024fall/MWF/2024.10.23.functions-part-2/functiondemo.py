3bed098ea18220928c9d54e05f91d95cdfdc2cbc
John Gibbons <jwgibbo@gmail.com>
2024-10-23 10:46:51 -0500

    Add 168 notes

---
+++ eecs168/2024fall/MWF/2024.10.23.functions-part-2/functiondemo.py
@@ -0,0 +1,20 @@
+#Goal: Make a function that prints
+#      the countdown to Halloween song
+
+def halloween_song(days):
+    print(days, 'more days til Halloween')
+    print('Halloween')
+    print('Halloween')
+    print('Silver Shamrock!')
+    return
+
+def no_return():
+    print('You called no_return')
+
+halloween_song(8)
+test = no_return()
+print('test =', test)
+
+
+test = print('hi')
+print('test =', test)
