f7ee76b18db4f7b83d810d71b471022dc81502fd
John Gibbons <jwgibbo@gmail.com>
2025-01-27 13:52:27 -0600

    Add 168 notes

---
+++ eecs168/2025spring/MWF/2025.01.27.input/userinput.py
@@ -0,0 +1,2 @@
+user_name = input('Enter your name: ')
+print('Hello', user_name)
---
+++ eecs168/2025spring/MWF/2025.01.27.input/variables.py
@@ -0,0 +1,11 @@
+age = 41
+name = 'John'
+print('I am', age, 'years old')
+print('In 47 years I\'ll be', age+47)
+print('My name is', name)
+
+a = 5
+b = a
+a = 10
+print('a =', a)
+print('b =', b)
