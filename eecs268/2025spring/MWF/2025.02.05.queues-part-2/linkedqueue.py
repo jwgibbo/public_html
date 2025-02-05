ea8918e949bd6b76eee4cb8dc60e016746f69871
John Gibbons <jwgibbo@gmail.com>
2025-02-05 10:49:13 -0600

    Add 268 notes

---
+++ eecs268/2025spring/MWF/2025.02.05.queues-part-2/linkedqueue.py
@@ -0,0 +1,19 @@
+#linkedqueue.py
+
+from node import Node
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def is_empty(self):
+        if self._front is None and self._back is None:
+            return True
+        else:
+            return False
+
+    def enqueue(self, entry):
+        #define this for board work
+            
+        
