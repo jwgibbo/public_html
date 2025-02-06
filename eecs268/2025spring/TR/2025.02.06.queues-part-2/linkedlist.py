af0010b5575cfe8a3421db970a5dffd7a310f986
John Gibbons <jwgibbo@gmail.com>
2025-02-06 13:00:45 -0600

    Add 268 notes

---
+++ eecs268/2025spring/TR/2025.02.06.queues-part-2/linkedlist.py
@@ -0,0 +1,16 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList(self):
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        #We'll do next week
+        #Assume it works for now
+
+    def get_entry(self, index):
+        #return the value at the index
+        #OR raise an IndexError
---
+++ eecs268/2025spring/TR/2025.02.06.queues-part-2/linkedqueue.py
@@ -0,0 +1,26 @@
+#linkedqueue.py
+
+from node import Node
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def is_empty(self):
+        if self._front is None and self._back is None:
+            return True
+        else:
+            return False
+
+    def enqueue(self, entry):
+        ???
+
+    def dequeue(self):
+        #Either return the dequeued value
+        #or raise a RuntimeError if empty
+
+    def peek_front(self):
+        #returns the front value OR
+        #raise a RuntimeError
+        
