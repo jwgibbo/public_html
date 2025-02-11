193e8f2d5f91f99e15cc2c36aa926a18872da11c
John Gibbons <jwgibbo@gmail.com>
2025-02-11 12:22:30 -0600

    Add 268 notes

---
+++ eecs268/2025spring/TR/2025.02.11.linked-lists/linkedlist.py
@@ -0,0 +1,18 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList(self):
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        #Define this today (Tues Feb 11)
+        #Insert the entry at the index OR
+        #raise an IndexError if the index
+        #if invalid
+
+    def get_entry(self, index):
+        #return the value at the index
+        #OR raise an IndexError
