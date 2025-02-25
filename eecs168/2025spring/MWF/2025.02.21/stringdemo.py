ca613154f56d577bda9fd6d70a1312cc68aab10b
John Gibbons <jwgibbo@gmail.com>
2025-02-25 10:40:51 -0600

    Create stringdemo.py

---
+++ eecs168/2025spring/MWF/2025.02.21/stringdemo.py
@@ -0,0 +1,21 @@
+#stringdemo.py
+
+word = 'FRIDAY'
+print(word[0])
+print(word[5])
+print('length =', len(word))
+print(word[0:3])
+print(word[3:6])
+print(word[2:])
+print(word[:4])
+print(word[0:6:2])
+print(word[-1])
+print(word[-1:-7:-1])
+print(word[::-1])
+print(word.lower())
+print(word)
+#num = 12345
+#print(len(num))
+
+
+
