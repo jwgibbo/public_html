b674f7ca3c7672c363a8f47832853970ba7c5730
John Gibbons <jwgibbo@gmail.com>
2025-02-25 14:21:40 -0600

    Add 168 notes

---
+++ eecs168/2025spring/TR/2025.02.25.strings-and-lists/stringdemo.py
@@ -0,0 +1,22 @@
+#stringdemo.py
+
+word = 'average'
+print(word)
+print(word[0])
+print(word[-1])
+print(len(word))
+print(word[-len(word)])
+# print(word[99]) ERROR
+print(word[0:3])
+print(word[3:7])
+print(word[0:7:4])
+print(word[2:99])
+print(word[5:])
+print(word[:4])
+print(word[:])
+print(word[-1:-8:-1])
+print(word[::-1])
+print(word.upper())
+print(word)
+print(word.count('a'))
+print(word.count('z'))
