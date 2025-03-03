e830e2621b8f8f83f196babc22e739cc7e4b8027
John Gibbons <jwgibbo@gmail.com>
2025-03-03 13:52:57 -0600

    Add 168 notes

---
+++ eecs168/2025spring/MWF/2025.03.03.lists-part-3/griddemo.py
@@ -0,0 +1,18 @@
+row1 = [10, 20, 30, 40]
+row2 = [50, 60, 70, 85]
+row3 = [80, 90, 100, 110]
+
+print(row1)
+print(row2)
+print(row3)
+
+grid = []
+grid.append(row1)
+grid.append(row2)
+grid.append(row3)
+print(grid)
+print(len(grid))
+print(type(grid))
+print(type(grid[0]))
+print(type(grid[0][0]))
+print(grid[1][2])
