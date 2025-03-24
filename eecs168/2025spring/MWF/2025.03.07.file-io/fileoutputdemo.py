9edaf8d8c6a1bd1c07d9a31983d5d3ead6daac49
John Gibbons <jwgibbo@gmail.com>
2025-03-24 13:55:30 -0500

    Add 168 notes

--- eecs168/2025spring/MWF/2025.03.07.file-io/fileoutputdemo.py
+++ eecs168/2025spring/MWF/2025.03.07.file-io/fileoutputdemo.py
@@ -1,6 +1,7 @@
 output_file = open('output.txt', 'w')
 
 output_file.write('testing testing\n')
-output_file.write('is this thing on?')
+output_file.write('is this thing on?\n')
+output_file.write(str(5))
 
 output_file.close()
