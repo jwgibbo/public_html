5419b6b66cc0da7d90e61f6caa2f43d66a4137ea
John Gibbons <jwgibbo@gmail.com>
2025-03-27 14:15:37 -0500

    Add 168/268 notes

---
+++ eecs168/2025spring/MWF/2025.03.26.functions/funcdemo.py
@@ -0,0 +1,17 @@
+#Goal: Define a function that takes in
+#       a number and returns that number
+#       plus one
+def plus_one(num):
+    answer = num + 1
+    return answer
+
+
+#Outside of the function
+#Let's call our function
+result = plus_one(5)
+print(result)
+print(plus_one(9) + plus_one(19))
+
+
+if is_even(user_num):
+    print('it is even!')
