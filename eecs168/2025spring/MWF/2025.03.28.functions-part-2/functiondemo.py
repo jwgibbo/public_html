905ccf204fd8e2a44de53925253a765302e565de
John Gibbons <jwgibbo@gmail.com>
2025-03-28 13:54:06 -0500

    Add 168 notes

---
+++ eecs168/2025spring/MWF/2025.03.28.functions-part-2/functiondemo.py
@@ -0,0 +1,11 @@
+#Goal making a function that welcomes
+#a given person
+
+def welcome(name):
+    print('Hello', name)
+    print('We are so happy you are here!')
+    return
+
+
+welcome('John Gibbons')
+welcome('Mario Mario')
