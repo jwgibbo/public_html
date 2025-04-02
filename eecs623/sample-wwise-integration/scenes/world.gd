b932eee31e0cee6dc2ce7b991c06dfabeba1969e
John Gibbons <jwgibbo@gmail.com>
2025-04-01 22:26:10 -0500

    Added demo for EECS 623

--- eecs623/sample-wwise-integration/scenes/world.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	Wwise.register_game_obj(self, self.name)
-	Wwise.register_listener(self)
-	Wwise.load_bank_id(AK.BANKS.DCP_THE_CORE)
-	Wwise.load_bank_id(AK.BANKS.MAIN)
-	Wwise.load_bank_id(AK.BANKS.MUSIC)
-	Wwise.post_event_id(AK.EVENTS.DEFEATED_BAUUL, self)
