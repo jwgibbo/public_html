96342ffc99776622fe53ca40b76605aeff722bb2
John Gibbons <jwgibbo@gmail.com>
2025-04-23 10:51:22 -0500

    Add 268 notes

---
+++ eecs268/2025spring/MWF/2025.04.23.complexity/loopdemo.py
@@ -0,0 +1,19 @@
+# ALGORITHM 1: Constant
+
+for num in range(10):
+    #do something
+
+
+# ALGORITHM 2: Linear
+
+n = ?
+for num in range(n):
+    #do something
+
+
+
+# ALGORITHM 3: Quadratic
+n = ?
+for i in range(n):
+    for j in range(n):
+        #do something
