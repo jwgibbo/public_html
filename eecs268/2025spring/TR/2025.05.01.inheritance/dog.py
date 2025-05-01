547b9971f5511968a5f6d9e11aab872048ccb1c5
John Gibbons <jwgibbo@gmail.com>
2025-05-01 12:15:49 -0500

    Add 268 notes

---
+++ eecs268/2025spring/TR/2025.05.01.inheritance/dog.py
@@ -0,0 +1,11 @@
+#dog.py
+
+from animal import Animal
+
+class Dog(Animal):
+    def do_trick(self):
+        print('Dog doing amazing trick!')
+
+    def sleep(self):
+        print('Dog sleeping...')
+        print('Hrp. Hrp HRP. Hrp hrrp.')
