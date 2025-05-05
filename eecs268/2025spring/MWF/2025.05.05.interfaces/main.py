f6abff24524a86e4b0541362a7654dd754ec6213
John Gibbons <jwgibbo@gmail.com>
2025-05-05 10:51:48 -0500

    Add 268 notes

---
+++ eecs268/2025spring/MWF/2025.05.05.interfaces/main.py
@@ -0,0 +1,12 @@
+#main.py
+
+from shape import Shape
+from triangle import Triangle
+
+def main():
+    #my_shape = Shape()
+    #print(my_shape.area())
+
+    my_tri = Triangle(5, 10)
+    print(my_tri.area())
+main()
