2e9d0fab9b510cbe0547556d06c52e2e5921e332
John Gibbons <jwgibbo@gmail.com>
2025-06-05 13:14:37 -0500

    Add 168 notes

---
+++ eecs168/2025summer/2025.06.05.input/inputdemo.py
@@ -0,0 +1,16 @@
+age = 41
+print(age)
+
+Age = 55
+print(Age)
+
+base = 5
+height = 10
+area = base*height*0.5
+print(area)
+
+user_name = input('What is your name?: ')
+print('Hello', user_name)
+
+user_age = int(input('What is your age?: '))
+print(user_age+10)
