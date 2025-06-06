bc36a05299ebb9acc303df558ee17292e5f67e48
John Gibbons <jwgibbo@gmail.com>
2025-06-06 14:20:53 -0500

    Add 168/268 notes

---
+++ eecs268/2025summer/2025.06.06.stacks-part-2/main.py
@@ -0,0 +1,14 @@
+#main.py
+
+from stack import Stack
+
+def main():
+    my_stack = Stack()
+    #We won't directly interact with Nodes
+    my_stack.push(5)
+    my_stack.push(10)
+    my_stack.push(15)
+    my_stack.pop()
+    my_stack.pop()
+    my_stack.pop()
+    my_stack.pop() #raise an error
---
+++ eecs268/2025summer/2025.06.06.stacks-part-2/stack.py
@@ -0,0 +1,18 @@
+#stack.py
+
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None #empty stack
+
+    def is_empty(self):
+        return self._top is None
+
+    def push(self, entry):
+        #define this (Thursday)
+
+    def pop(self):
+        #define this (Friday)
+        #Remove the top node and return the popped value
+        #OR raise a RuntimeError if the stack is empty
