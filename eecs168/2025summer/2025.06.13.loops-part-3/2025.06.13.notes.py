2681ab12c080c36d70083d50de014270ab1f3284
John Gibbons <jwgibbo@gmail.com>
2025-06-13 13:56:07 -0500

    Add 168/268 notes

---
+++ eecs168/2025summer/2025.06.13.loops-part-3/2025.06.13.notes.py
@@ -0,0 +1,18 @@
+word = 'FRIDAY'
+
+index = 0
+while index < len(word):
+    print(word[index])
+    index = index + 1
+
+
+print('--------')
+
+for letter in word:
+    print(letter)
+
+
+print('--------')
+
+for index in range(0, len(word)):
+    print(word[index])
