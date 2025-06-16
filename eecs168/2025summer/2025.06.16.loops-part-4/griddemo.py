3dcfae94e84e893ac14ff39ff89f566cf37d6cd8
John Gibbons <jwgibbo@gmail.com>
2025-06-16 13:06:41 -0500

    Add 168 notes

---
+++ eecs168/2025summer/2025.06.16.loops-part-4/griddemo.py
@@ -0,0 +1,16 @@
+#Goal: Print a 3x4 (3 rows of 4 cols) grid of *'s
+
+rows = 30
+cols = 40
+
+for row in range(rows):
+    for col in range(cols):
+        print('*', end='')
+
+    print('')
+
+
+
+
+
+
---
+++ eecs168/2025summer/2025.06.16.loops-part-4/nestedloopdemo.py
@@ -0,0 +1,8 @@
+print('i\tj')
+print('=============')
+
+for i in range(0, 3):
+    print('Entered outer loop')
+    for j in range(10, 14):
+        print(i, '\t', j)
+        
