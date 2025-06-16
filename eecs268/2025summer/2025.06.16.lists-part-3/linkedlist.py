945f6ab0408a7fb78b938c70c889438de23801b7
John Gibbons <jwgibbo@gmail.com>
2025-06-16 14:20:30 -0500

    Add 168/268 notes

---
+++ eecs268/2025summer/2025.06.16.lists-part-3/linkedlist.py
@@ -0,0 +1,26 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        #define this last week
+
+    def get_entry(self, index):
+        #defined this week
+
+    def remove(self, index):
+        #defined Monday
+
+    def length(self):
+        return self._length
+
+    def set_entry(self, index, entry):
+        #define for lab
+
+    def clear(self):
+        #define for lab
