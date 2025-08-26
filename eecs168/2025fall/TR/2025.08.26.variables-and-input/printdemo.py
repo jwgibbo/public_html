f50a945437d5894589dcc70cbed1502ad68d56fd
John Gibbons <jwgibbo@gmail.com>
2025-08-26 10:44:33 -0500

    Add 168 notes

---
+++ eecs168/2025fall/TR/2025.08.26.variables-and-input/printdemo.py
@@ -0,0 +1,9 @@
+print("I can't")
+print('They said "Hi."')
+print("They said \"I can't\"")
+print('They said "I can\'t"')
+print('This is a backslash \\')
+print('See below:\n\tHi >:D')
+print('\\n')
+
+
---
+++ eecs168/2025fall/TR/2025.08.26.variables-and-input/varaibledemo.py
@@ -0,0 +1,6 @@
+user_name = input('What is your name?: ')
+user_age = input('What is your age?: ')
+
+print(user_name)
+print(type(user_age))
+print('In ten year I will be', user_age+10)
