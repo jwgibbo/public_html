659754f43bb13aabef997414fcdad31ec4bab2b9
John Gibbons <jwgibbo@gmail.com>
2025-08-27 12:51:24 -0500

    Add 268 notes

---
+++ eecs268/2025fall/MWF/2025.08.27.stacks-part-2/stack.py
@@ -0,0 +1,22 @@
+#stack.py
+
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None #empty stack
+
+    def is_empty(self):
+        if self._top is None:
+            return True
+        else:
+            return False
+
+    def push(self, entry):
+        #Create a new Node; add entry
+        #Make it top-most node without
+        #losing track of current top
+
+    def pop(self):
+        #define this (Wednesday)
+        
---
+++ eecs268/2025fall/TR/2025.08.26.nodes-and-stack/main.py
@@ -0,0 +1,10 @@
+#main.py
+
+from node import Node
+
+def main():
+    first = Node('A')
+    print(first.entry)
+    print(first.next)
+
+main()
