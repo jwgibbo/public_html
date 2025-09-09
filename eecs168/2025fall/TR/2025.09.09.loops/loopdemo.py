56a609b0c6e73c065e5e73478c58b4f4971ec0ad
John Gibbons <jwgibbo@gmail.com>
2025-09-09 12:20:01 -0500

    Add 168/268 notes

---
+++ eecs168/2025fall/TR/2025.09.09.loops/loopdemo.py
@@ -0,0 +1,7 @@
+#Goal: Print the numbers 1 to 5
+num = 1
+
+while num <= 5:
+    print(num)
+    num = num + 1
+    
---
+++ eecs268/2025fall/TR/2025.09.09.queues-and-lists/linkedqueue.py
@@ -0,0 +1,23 @@
+#linkedqueue.py
+
+from node import Node
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def is_empty(self):
+        if self._front is None and self._back is None:
+            return True
+        else:
+            return False
+
+    def enqueue(self, entry):
+         #define (Thur Sep 4)
+
+    def dequeue(self):
+        #define (Thur Sep 4)
+
+    def peek_front(self):
+        #define (Tue Sep 9)
