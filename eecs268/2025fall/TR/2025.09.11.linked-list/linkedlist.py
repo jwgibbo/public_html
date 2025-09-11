bb0c73978ddbba9de678a8111ddffa3f4e95d0cf
John Gibbons <jwgibbo@gmail.com>
2025-09-11 12:23:26 -0500

    Add 168/268 notes

---
+++ eecs268/2025fall/TR/2025.09.11.linked-list/linkedlist.py
@@ -0,0 +1,17 @@
+#linkedlist
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def length(self):
+        return self._length
+
+    def insert(self, index, entry):
+        #define Thur Sep 11
+
+    def get_entry(self, index):
+        #define Thur Sep 11
