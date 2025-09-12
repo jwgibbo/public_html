81763e512ae0c1ebb692b8055ce4c68351cd9fec
John Gibbons <jwgibbo@gmail.com>
2025-09-12 10:51:25 -0500

    Add 168 notes

---
+++ eecs168/2025fall/MWF/2025.09.12.ranges/rangedemo.py
@@ -0,0 +1,17 @@
+for num in range(4):
+    print(num)
+
+print('---------')
+
+for num in range(1, 6):
+    print(num)
+
+print('---------')
+
+for num in range(4, 13, 2):
+    print(num)
+
+print('---------')
+
+for num in range(5, -6, -1):
+    print(num)
