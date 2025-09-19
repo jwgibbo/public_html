3b1c201654c78631610d4fe85a59fcfa01bd4008
John Gibbons <jwgibbo@gmail.com>
2025-09-19 11:04:15 -0500

    Add 168 code/notes

---
+++ eecs168/2025fall/MWF/2025.09.19.lists/listdemo.py
@@ -0,0 +1,13 @@
+#Goal: Obtain 50 ints from the user
+
+nums = [5, 10, 15, 20]
+
+print(nums)
+print(nums[0])
+print(nums[3])
+print('--------')
+
+nums[0] = 99
+nums.append(55)
+for num in nums:
+    print(num)
