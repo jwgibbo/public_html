f9e4c9bff9a2e5c38eee697479d539f8b6cf594b
John Gibbons <jwgibbo@gmail.com>
2025-09-22 12:47:01 -0500

    Add 268 notes

---
+++ eecs268/2025fall/MWF/2025.09.22.list-exercises/linkedlist.py
@@ -0,0 +1,27 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def remove(self, index):
+        #define Fri Sep 19
+
+    def insert(self, index, entry):
+        #define Wed Sep 17
+        
+    def get_entry(self, index):
+        #define Fri Sep 12
+
+    def set_entry(self, index, entry):
+        #define Fri Sep 12
+
+    def length(self):
+        #define Fri Sep 12
+
+    def clear(self):
+        #define Mon Sep 15
+        
