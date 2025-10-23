416045e0bc252e637fd88a4b3924d9b764d9604a
John Gibbons <jwgibbo@gmail.com>
2025-10-23 10:51:27 -0500

    Add 168 notes

---
+++ eecs168/2025fall/TR/2025.10.23.functions/functiondemo.py
@@ -0,0 +1,12 @@
+#Goal: Define a function that takes in
+#       a number and returns one more
+#       than that number
+def one_up(num):
+    #you don't need to give num a value
+    answer = num + 1
+    #you don't need to print
+    return answer
+
+print(one_up(4))
+result = one_up(99)+one_up(4)-one_up(19)
+print(result)
