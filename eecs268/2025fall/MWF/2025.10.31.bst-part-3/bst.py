91eeaed04020c915c296b316bff132dd31916ef7
John Gibbons <jwgibbo@gmail.com>
2025-10-31 12:51:58 -0500

    Add 268 notes

---
+++ eecs268/2025fall/MWF/2025.10.31.bst-part-3/bst.py
@@ -0,0 +1,16 @@
+#bst.py
+
+from binarynode import BinaryNode
+
+class BST:
+    def __init__(self):
+        self._root = None
+
+    def add(self, entry):
+        pass #assume this works
+
+    def search(self, key):
+        return self._rec_search(key, self._root)
+
+    def _rec_search(self, key, cur_node):
+        #define on all hallows eve
