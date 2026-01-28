dc35e0aeb34599e8802f3ed544bd3c2127d656b1
John Gibbons <jwgibbo@gmail.com>
2026-01-28 10:52:49 -0600

    Add 268 notes

--- eecs268/2026spring/MWF/2026.01.26.nodes/main.py
+++ eecs268/2026spring/MWF/2026.01.26.nodes/main.py
@@ -4,7 +4,11 @@ from node import Node
 
 def main():
     first = Node('A')
-    temp = ???
+    temp = Node('B')
+    first.next = temp
+    temp = Node('C')
+    first.next.next = temp
+    print(first.next.next)
     #finish coding main to match the drawing
 
 
---
+++ eecs268/2026spring/MWF/2026.01.28.stacks/main.py
@@ -0,0 +1,9 @@
+#main.py
+
+from stack import Stack
+
+def main():
+    my_stack = Stack()
+    my_stack.push(5)
+    my_stack.push(10)
+    my_stack.pop()
