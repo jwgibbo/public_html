8a851f83425c0765c5f50224eb2fda6abe1009e3
John Gibbons <jwgibbo@gmail.com>
2026-01-30 10:55:18 -0600

    Add 168/268 notes

---
+++ eecs168/2026spring/MWF/2026.01.28.variables/variabledemo.py
@@ -0,0 +1,8 @@
+age = input('Enter your age: ')
+print(age)
+print(age+5)
+print('age = ', age)
+age = 55
+print(age)
+name = input('What is your name?: ')
+print(name)
--- eecs268/2026spring/MWF/2026.01.28.stacks/main.py
+++ eecs268/2026spring/MWF/2026.01.28.stacks/main.py
@@ -6,4 +6,3 @@ def main():
     my_stack = Stack()
     my_stack.push(5)
     my_stack.push(10)
-    my_stack.pop()
