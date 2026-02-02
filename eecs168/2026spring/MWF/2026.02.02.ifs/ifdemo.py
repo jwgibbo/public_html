2a999a28b2ae18b8bdbf3a1471a36b56550a8d86
John Gibbons <jwgibbo@gmail.com>
2026-02-02 13:50:16 -0600

    Add 168/268 notes

---
+++ eecs168/2026spring/MWF/2026.02.02.ifs/ifdemo.py
@@ -0,0 +1,11 @@
+num = 0
+
+print('Program starting...')
+
+if num > 0:
+    print('Yay it is positive!')
+else:
+    print('Oh no!')
+    print('Not positive :*(')
+    
+print('Program ending...')
---
+++ eecs268/2026spring/MWF/2026.02.02.stack-exercises/stack.py
@@ -0,0 +1,21 @@
+#stack.py
+
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self._top = None
+
+    
+    def push(self, entry):
+        #board work Friday
+
+    def pop(self):
+        #board work Friday
+
+
+    def peek(self):
+        #board work Monday
+
+    def is_empty(self):
+        #board work Monday
