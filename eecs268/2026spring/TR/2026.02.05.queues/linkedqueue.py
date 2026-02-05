0d6ce6d4aac1d977e79d8ab21b9526b201713515
John Gibbons <jwgibbo@gmail.com>
2026-02-05 12:18:09 -0600

    Add 268 notes

---
+++ eecs268/2026spring/TR/2026.02.05.queues/linkedqueue.py
@@ -0,0 +1,18 @@
+#linkedqueue.py
+
+class LinkedQueue:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def is_empty(self):
+        if self._front is None and self._back is None:
+            return True
+        else:
+            return False
+
+    def enqueue(self, entry):
+        #define this 2026.02.05
+
+    def dequeue(self):
+        #define this 2026.02.05
