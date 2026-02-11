9fef01669963f9ca08cd3eb2558d37d9b598ba76
John Gibbons <jwgibbo@gmail.com>
2026-02-11 13:54:46 -0600

    Add 168 notes

---
+++ eecs168/2026spring/MWF/2026.02.11.for-in-part-2/rangedemo.py
@@ -0,0 +1,20 @@
+for num in range(5):
+    print(num)
+
+print('-------')
+
+for num in range(2, 6):
+    print(num)
+
+print('--------')
+
+for num in range(3, 10, 2):
+    print(num)
+
+print('--------')
+
+for num in range(10, 5, -2):
+    print(num)
+
+print('testing', end='')
+print('testing')
