8d46cd287755595a1987e8c98c5c97de7b4aba97
John Gibbons <jwgibbo@gmail.com>
2026-02-13 10:52:51 -0600

    Add 268 notes

---
+++ eecs268/2026spring/MWF/2026.02.13.lists-part-3/linkedlist.py
@@ -0,0 +1,20 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def get_entry(self, index):
+        #return the entry at the index
+        #raise an IndexError if index is invalid
+    
+    def set_entry(self, index, entry):
+        #replace the entry at the index
+        #OR raise and IndexError
+
+    def insert(self, index, entry):
+        #Either add the new entry at the index
+        #OR raise IndexError
