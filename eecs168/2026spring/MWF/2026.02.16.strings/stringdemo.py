fc1bb7add937cfb81ebfef2c0124065d3f5e3479
John Gibbons <jwgibbo@gmail.com>
2026-02-16 13:51:15 -0600

    Add 168 notes

---
+++ eecs168/2026spring/MWF/2026.02.16.strings/stringdemo.py
@@ -0,0 +1,18 @@
+word = 'sunshine'
+print(word[0])
+print(word[7])
+print(len(word))
+
+# word[0] = 'f' ERROR
+
+print(word[0: 3])
+print(word[4: 7])
+print(word[0: 6: 2])
+print(word[3:])
+print(word[:5])
+print(word[::])
+
+#negative indexing
+print(word[-1])
+print(word[-1:-9:-1])
+print(word[::-1])
