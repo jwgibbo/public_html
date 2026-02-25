dcc87ffdcb34ead370ef07488cbf9a66d5ec524f
John Gibbons <jwgibbo@gmail.com>
2026-02-25 13:49:45 -0600

    Add 168 notes

---
+++ eecs168/2026spring/MWF/2026.02.25.lists-part-3/listofstring.py
@@ -0,0 +1,9 @@
+words = ['cat', 'dog', 'bat', 'bird']
+
+for word in words:
+    print(word[0])
+
+print('------')
+print(words[1].upper())
+print(words[1][0])
+print(words[2][2])
---
+++ eecs168/2026spring/MWF/2026.02.25.lists-part-3/listsoflists.py
@@ -0,0 +1,13 @@
+#Goal: Make a 3x4 grid of numbers
+
+grid = []
+grid.append( [5, 10, 15, 20] )
+grid.append( [6, 12, 18, 24] )
+grid.append( [7, 14, 21, 28] )
+
+print(grid)
+print(len(grid))
+print(grid[0])
+print(grid[1][2])
+print(grid[2][3])
+#print(grid[3][2])
