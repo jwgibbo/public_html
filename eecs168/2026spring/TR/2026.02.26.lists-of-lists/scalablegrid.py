8609dfa419bd7fd5e2ff64aa57933f5db3d18d9e
John Gibbons <jwgibbo@gmail.com>
2026-03-03 13:47:39 -0600

    Add 168 notes

--- eecs168/2026spring/TR/2026.02.26.lists-of-lists/scalablegrid.py
+++ eecs168/2026spring/TR/2026.02.26.lists-of-lists/scalablegrid.py
@@ -1,9 +1,9 @@
 #Let's create 3 rows of 5 numbers from the user
 grid = [] #make an empty grid
-row = [] #make an empty row
+
 
 for row_num in range(3):
-
+    row = [] #make an empty row
     for col_num in range(5):
         user_input = int(input('Enter a number: '))
         row.append(user_input)
---
+++ eecs168/2026spring/TR/2026.03.03.strings-to-list/stringtolists.py
@@ -0,0 +1,13 @@
+fruits = 'apple+kiwi+mango'
+
+fruit_list = list(fruits)
+
+print(fruit_list)
+print(len(fruit_list))
+
+fruit_list = fruits.split('+')
+print(fruit_list)
+
+delimiter = ' & '
+fruit_string = delimiter.join(fruit_list)
+print(fruit_string)
