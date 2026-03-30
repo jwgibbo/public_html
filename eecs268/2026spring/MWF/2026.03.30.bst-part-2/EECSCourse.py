307f5f7f49005a8489229d47799ac6c3f9c49e76
John Gibbons <jwgibbo@gmail.com>
2026-03-30 10:52:53 -0500

    Add 268 notes

---
+++ eecs268/2026spring/MWF/2026.03.30.bst-part-2/EECSCourse.py
@@ -0,0 +1,18 @@
+#EECS_Course
+
+
+class EECS_Course:
+    def __init__(self):
+        self.course_num = ???
+        self.prof = ???
+        self.credits = ???
+        self.required = ???
+
+    def __lt__(self, other):
+        #More on this on Wednesday
+
+    def __gt__(self, other):
+        #More on this on Wednesday
+
+    def __eq__(self, other):
+        #More on this on Wednesday
