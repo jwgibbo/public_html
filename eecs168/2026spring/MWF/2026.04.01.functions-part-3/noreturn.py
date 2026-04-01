e957983b8a1c6e8e96c70cf88143a3ceada152d5
John Gibbons <jwgibbo@gmail.com>
2026-04-01 13:53:41 -0500

    Add 168 notes

---
+++ eecs168/2026spring/MWF/2026.04.01.functions-part-3/noreturn.py
@@ -0,0 +1,11 @@
+def greeting(name):
+    print('Hello', name)
+    print('Glad you are here!')
+
+
+def main():
+    greeting('John')
+    greeting('Jane')
+    greeting('Xanathar the World Eater')
+
+main()
--- eecs268/2026spring/MWF/2026.04.01.bst-part-3/bst.py
+++ eecs268/2026spring/MWF/2026.04.01.bst-part-3/bst.py
@@ -6,8 +6,26 @@ class BST:
     def __init__(self):
         self._root = None
 
-    def search(self, target):
-        return self._rec_search(target, self._root)
+    def search(self, key):
+        return self._rec_search(key, self._root)
 
-    def _rec_search(self, target, cur_node):
+    def _rec_search(self, key, cur_node):
         #define this Monday Mar 30th
+
+    def add(self, entry):
+        if self._root is None:
+            self._root = BinaryNode(entry)
+        else:
+            self._rec_add(entry, self._root)
+
+    def _rec_add(self, entry, cur_node):
+        #define this Wed Apr 1st
+        
+'''OLD BUSTED VERSION
+    def add(self, entry):
+        self._rec_add(entry, self._root)
+
+    def _rec_add(self, entry, cur_node):
+        if cur_node is None:
+            cur_node = BinaryNode(entry)
+'''
