80e8e2088d9335185003c6cbcfcf1a73f14808d7
John Gibbons <jwgibbo@gmail.com>
2026-04-15 11:15:07 -0500

    Add 268 files

---
+++ eecs268/2026spring/MWF/2026.04.15.complexity/complexitydemo.py
@@ -0,0 +1,19 @@
+#complexitydemo.py
+
+# n is our problem size
+n = ???
+
+
+# Algorithm 0: Constant scaling O(1)
+for num in range(55):
+    #do something
+
+# Algorithm 1: Scales linearly  O(n)
+for num in range(n):
+    #do something
+
+
+# Algorithm 2: Scales quadraticly O(n^2)
+for outer in range(n):
+    for inner in range(n):
+        #do something
