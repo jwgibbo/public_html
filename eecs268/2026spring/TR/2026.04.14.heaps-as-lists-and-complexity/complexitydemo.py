cdc644098da80ed61fbf8c9007e34d1f9100bfc9
John Gibbons <jwgibbo@gmail.com>
2026-04-16 12:20:25 -0500

    Add 268 notes

--- eecs268/2026spring/TR/2026.04.14.heaps-as-lists-and-complexity/complexitydemo.py
+++ eecs268/2026spring/TR/2026.04.14.heaps-as-lists-and-complexity/complexitydemo.py
@@ -1,16 +1,22 @@
 
 
-n = ???
+n = 1_000_000
 
 # Algorithm 0: Scales in a constant way
 for num in range(50):
-    #do something
+    pass
+
+print('Algorithm 0 finished')
 
 # Algorithm 1: Scales linearly
 for num in range(n):
-    #do something
+    pass
+
+print('Algorithm 1 finished')
 
 # Algorithm 2: Scales Quadratically
 for outer in range(n):
     for inner in range(n):
-        #do something
+        pass
+
+print('Algorithm 2 finished')
