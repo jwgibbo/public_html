2456cf61c34c881ec294aa042ab7d277c14ee93a
John Gibbons <jwgibbo@gmail.com>
2026-04-20 13:01:33 -0500

    Add 268 notes

---
+++ eecs268/2026spring/MWF/2026.04.20.inheritance/animal.py
@@ -0,0 +1,11 @@
+#animal.py
+
+class Animal:
+    def __init__(self):
+        print('Animal made!')
+
+    def eat(self):
+        print('Animal eating...')
+
+    def sleep(self):
+        print('Animal sleeping')
---
+++ eecs268/2026spring/MWF/2026.04.20.inheritance/main.py
@@ -0,0 +1,10 @@
+#main.py
+
+from animal import Animal
+from dog import Dog
+
+def main():
+    my_animal = Animal()
+    my_dog = Dog()
+
+main()
