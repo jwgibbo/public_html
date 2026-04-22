c33c67a4fde6c8f38e476a7edc2b53f36c17646d
John Gibbons <jwgibbo@gmail.com>
2026-04-22 10:51:32 -0500

    Add 268 notes

---
+++ eecs268/2026spring/MWF/2026.04.22.inheritance-part-2/dog.py
@@ -0,0 +1,10 @@
+#dog.py
+
+from animal import Animal
+
+class Dog(Animal):
+    def do_trick(self):
+        print('Dog doing amazing trick. WOW!')
+
+    def eat(self):
+        print('Dog is eating...')
