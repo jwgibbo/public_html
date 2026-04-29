93789d53e09cc7fedc77439308f38a0233b4e5ab
John Gibbons <jwgibbo@gmail.com>
2026-04-29 10:55:32 -0500

    Add 168/268 notes

--- eecs268/2026spring/MWF/2026.04.24.iinhertiance-and-nterfaces/animal/main.py
+++ eecs268/2026spring/MWF/2026.04.24.iinhertiance-and-nterfaces/animal/main.py
@@ -3,16 +3,18 @@
 from animal import Animal
 from dog import Dog
 
+def play_with_animal(some_animal):
+    some_animal.eat()
+    some_animal.sleep()
+
 def main():
     my_animal = Animal()
     my_dog = Dog()
 
-    my_animal.eat()
-    my_animal.sleep()
+    play_with_animal(my_animal)
     #my_animal.do_trick() ERROR
 
-    my_dog.eat()
-    my_dog.sleep()
+    play_with_animal(my_dog)
     my_dog.do_trick()
 
 main()
