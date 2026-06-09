f599c8256acc590a787f22dc5b4f9dad7a1f32e7
John Gibbons <jwgibbo@gmail.com>
2026-06-09 11:26:57 -0500

    Add 268 notes

---
+++ eecs268/2026summer/2026.06.09.queues-and-lists/lists/linkedlist.py
@@ -0,0 +1,14 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def insert(self, index, entry):
+        #We'll do this in a later lecture
+
+    def get_entry(self, index):
+        #define this
