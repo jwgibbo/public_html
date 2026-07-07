8dcb7cd1061f9cfb28646b3d8b91161fa4153477
John Gibbons <jwgibbo@gmail.com>
2026-07-07 11:40:44 -0500

    Add 268 notes

---
+++ eecs268/2026summer/2026.07.07.binary-search-tree/bst.py
@@ -0,0 +1,32 @@
+#bst.py
+
+from binarynode import BinaryNode
+
+class BST:
+    def __init__(self):
+        self._root = None
+
+    def add(self, entry):
+        if self._root is None:
+            self._root = BinaryNode(entry)
+        else:
+            self._rec_add(entry, self._root)
+            
+    def _rec_add(self, entry, cur_node):
+        #define this (Tuesday)
+
+    def search(self, key):
+        return self._rec_search(key, self._root)
+
+    def _rec_search(self, key, cur_node):
+        if cur_node is None:
+            raise KeyError('Target not found')
+        elif cur_node.entry == key:
+            return cur_node.entry
+        elif cur_node.entry > key:
+            return self._rec_search(key, cur_node.left)
+        elif cur_node.entry < key:
+            return self._rec_search(key. cur_node.right)
+        
+            
+            
