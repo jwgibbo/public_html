0b3efb626a4ff7e3ad4d910798ad6fe8eff61e61
John Gibbons <jwgibbo@gmail.com>
2026-07-15 11:49:29 -0500

    Add 268 notes

---
+++ eecs268/2026summer/2026.07.15.heaps-as-lists/minheap.py
@@ -0,0 +1,15 @@
+#minheap.py
+
+
+class MinHeap:
+    def __init__(self):
+        self._heap = []
+
+    def add(self, entry):
+        self._heap.append(entry)
+
+        self._upheap(len(self._heap)-1)
+
+
+    def _upheap(self, index):
+        #recursively upheaps the value at the index
