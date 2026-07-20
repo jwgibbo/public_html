fcd125646c225253a88b3638028a0e19e7a70a2c
John Gibbons <jwgibbo@gmail.com>
2026-07-20 11:54:41 -0500

    Add 268 notes

---
+++ eecs268/2026summer/2026.07.20.complexity/complexitydemo.py
@@ -0,0 +1,22 @@
+#Algorithm 0: Constant O(1)
+for num in range(10):
+    #do something
+    pass
+print('Algo 0 finished!')
+
+n = 1_000_000_000
+
+#Algorithm 1: Linear O(n)
+for num in range(n):
+    #do something
+    pass
+
+print('Algo 1 finished!')
+
+#Algorithm 2: Quadtratic O(n^2)
+for outer in range(n):
+    for inner in range(n):
+        #do something
+        pass
+
+print('Algo 2 finished!')
