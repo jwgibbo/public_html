e2cfe6725ad49391789b24aae048f5b7c93c3cf5
John Gibbons <jwgibbo@gmail.com>
2026-08-31 10:50:45 -0500

    Add 168 notes

---
+++ eecs168/2026fall/MWF/2026.08.31.variable-and-types/variabledemo.py
@@ -0,0 +1,8 @@
+age = 42
+print('I am', age)
+print('In 27 years I will be', age+27)
+print(type(age))
+
+user_age = int(input('How old are you?: '))
+print('You are', user_age)
+print('In 27 years you will be', user_age+27)
