fe64a1a84756c227074b48448335f79d3d9ae7a9
John Gibbons <jwgibbo@gmail.com>
2026-09-11 12:51:59 -0500

    Add 168/268 notes

---
+++ eecs168/2026fall/MWF/2026.09.11.exercises-more-strings/stringdemo.py
@@ -0,0 +1,12 @@
+phrase = 'Top row\nBottom row'
+print(phrase)
+headers = 'Mon\tTue\tWed\tThur\tFri'
+print(headers)
+
+phrase = "He said, \"I can\'t see it\""
+print(phrase)
+print('this is a backslash \\')
+
+num = 5
+print(f'The number is {num}')
+print(f'5+3 = {5+3}')
---
+++ eecs268/2026fall/MWF/2026.09.11.lists/linkedlist.py
@@ -0,0 +1,8 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._length = 0
+        self._front = None
