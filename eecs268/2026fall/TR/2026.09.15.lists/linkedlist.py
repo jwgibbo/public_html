d0ca93133eecb18de2894f4116d8d098cd3002e0
John Gibbons <jwgibbo@gmail.com>
2026-09-15 12:19:18 -0500

    Add 268 notes

---
+++ eecs268/2026fall/TR/2026.09.15.lists/linkedlist.py
@@ -0,0 +1,26 @@
+#linkedlist.py
+
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self._front = None
+        self._length = 0
+
+    def length(self):
+        return self._length
+
+    def clear(self):
+        self._front = None
+        self._length = 0
+
+    def get_entry(self, index):
+        #define Tue Sep 15
+
+    def set_entry(self, index, entry):
+        #define Tue Sep 15
+
+    def insert(self, index, entry):
+        #define Tue Sep 15
+        
+        
