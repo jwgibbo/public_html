91181e46f0667e8cd678a7a6d6ccc465ca3aa53d
John Gibbons <jwgibbo@gmail.com>
2026-09-16 11:06:28 -0500

    Add 168 notes

---
+++ eecs168/2026fall/MWF/2026.09.16.loops-part-2/breakdemo.py
@@ -0,0 +1,11 @@
+# Goal: Print the numbers 1 to 99
+
+num = 1
+
+while True:
+    print(num)
+    num = num + 1
+    if num >= 100:
+        break
+
+print('loop over')
---
+++ eecs168/2026fall/MWF/2026.09.16.loops-part-2/forindemo.py
@@ -0,0 +1,6 @@
+word = 'bird'
+print(type(word))
+print('length =', len(word))
+
+for letter in word:
+    print(letter)
