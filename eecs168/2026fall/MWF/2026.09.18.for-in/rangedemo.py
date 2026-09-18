9e5e7e97377cce66750e8521536cbe8d21f3822c
John Gibbons <jwgibbo@gmail.com>
2026-09-18 10:50:40 -0500

    Add 168 notes

---
+++ eecs168/2026fall/MWF/2026.09.18.for-in/rangedemo.py
@@ -0,0 +1,17 @@
+for num in range(5):
+    print(num)
+
+print('-----------')
+
+for num in range(3, 7):
+    print(num)
+
+print('-----------')
+
+for num in range(4, 11, 2):
+    print(num)
+
+print('-----------')
+
+for num in range(10, 5):
+    print(num)
